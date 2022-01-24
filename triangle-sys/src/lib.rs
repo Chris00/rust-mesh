88891f28740db810599ac59aa5c84c68bcd4f400
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2022-01-24 22:32:03 +0100

    triangle-sys: enable to define our own `triunsuitable` fn & test

--- triangle-sys/src/lib.rs
+++ triangle-sys/src/lib.rs
@@ -1,6 +1,22 @@
 //! Low-level bindings to [Triangle][].
 //!
-//! [Triangle]: http://www.cs.cmu.edu/~quake/triangle.html
+//! ## Triunsuitable
+//!
+//! The triangle library is compiled with `EXTERNAL_TEST` which means
+//! that you can define your own `triunsuitable` function.  In otder
+//! to do that, activate the `triunsuitable` feature of this crate and
+//! write a function (replacing `todo!()` with actual code 😀):
+//!
+//! ```no_run
+//! #[no_mangle]
+//! extern "C" fn triunsuitable(triorg: &[f64; 2], tridest: &[f64; 2],
+//!                             triapex: &[f64; 2], area: f64) -> bool {
+//!     todo!()
+//! }
+//! ```
+//!
+//! This function returns `true` if the triangle is too large and
+//! should be refined, `false` otherwise.
 //!
 //! ## License of the Triangle C library
 //!
@@ -19,8 +35,11 @@
 //! this code to a customer, and you are instead telling them how they
 //! can obtain it for free, then you are not required to make any
 //! arrangement with me.)
+//!
+//! [Triangle]: http://www.cs.cmu.edu/~quake/triangle.html
 
-use ::std::os::raw::{c_char, c_int};
+use ::std::{os::raw::{c_char, c_int},
+            ptr};
 
 /// Structure to pass data into and out of the [`triangulate`] function.
 ///
@@ -36,7 +55,7 @@ pub struct triangulateio {
     /// Array of point coordinates.  The first point's x coordinate is
     /// at index `0` and its y coordinate at index `1`, followed by
     /// the coordinates of the remaining points.  Each point occupies
-    /// two `f64`
+    /// two `f64` and they must both be *finite*.
     pub pointlist: *mut f64,
     /// An array of point attributes.  Each point's attributes occupy
     /// `numberofpointattributes` floats `f64`.
@@ -45,6 +64,7 @@ pub struct triangulateio {
     pub pointmarkerlist: *mut c_int,
     pub numberofpoints: c_int,
     pub numberofpointattributes: c_int,
+
     /// An array of triangle corners.  The first triangle's first
     /// corner is at index `0`, followed by its other two corners in
     /// counterclockwise order, followed by any other nodes if the
@@ -63,6 +83,7 @@ pub struct triangulateio {
     pub numberoftriangles: c_int,
     pub numberofcorners: c_int,
     pub numberoftriangleattributes: c_int,
+
     /// An array of segment endpoints.  The first segment's endpoints
     /// are at indices `0` and `1`, followed by the remaining
     /// segments.  Two ints per segment.
@@ -70,12 +91,14 @@ pub struct triangulateio {
     /// An array of segment markers; one int per segment.
     pub segmentmarkerlist: *mut c_int,
     pub numberofsegments: c_int,
+
     /// An array of holes.  The first hole's x and y coordinates are
     /// at indices `0` and `1`, followed by the remaining holes.  Two
     /// floats per hole.  Input only, although the pointer is copied
     /// to the output structure for your convenience.
     pub holelist: *mut f64,
     pub numberofholes: c_int,
+
     /// An array of regional attributes and area constraints.  The
     /// first constraint's x and y coordinates are at indices `0` and
     /// `1`, followed by the regional attribute at index `2`, followed
@@ -89,6 +112,7 @@ pub struct triangulateio {
     /// structure for your convenience.
     pub regionlist: *mut f64,
     pub numberofregions: c_int,
+
     /// An array of edge endpoints.  The first edge's endpoints are at
     /// indices `0` and `1`, followed by the remaining edges.  Two
     /// ints per edge.  **Output only.**
@@ -105,7 +129,8 @@ pub struct triangulateio {
 }
 
 extern "C" {
-    /// Triangulate the PSLG or refine the mesh.
+    /// Triangulate the PSLG or refine the mesh (depending on the
+    /// switches).
     ///
     /// `triswitches` string containing the command line switches you
     /// wish to invoke.  No initial dash is required.
@@ -115,9 +140,93 @@ extern "C" {
     /// switch is not used, `vorout` may be NULL.  `in` and `out` may
     /// never be NULL.
     pub fn triangulate(
-        triswitches: *mut c_char,
-        in_: *mut triangulateio,
+        triswitches: *const c_char,
+        in_: *const triangulateio,
         out: *mut triangulateio,
         vorout: *mut triangulateio,
     );
+}
+
+
+
+/// Default `triunsuitable` function.
+///
+/// This function is a Rust transcription of the default one used in
+/// Triangle code.
+#[cfg(not(feature = "triunsuitable"))]
+#[no_mangle]
+extern "C" fn triunsuitable(triorg: &[f64; 2], tridest: &[f64; 2],
+                 triapex: &[f64; 2], _area: f64) -> bool {
+    let dxoa = triorg[0] - triapex[0];
+    let dyoa = triorg[1] - triapex[1];
+    let dxda = tridest[0] - triapex[0];
+    let dyda = tridest[1] - triapex[1];
+    let dxod = triorg[0] - tridest[0];
+    let dyod = triorg[1] - tridest[1];
+    // Find the squares of the lengths of the triangle's three edges.
+    let oalen = dxoa * dxoa + dyoa * dyoa;
+    let dalen = dxda * dxda + dyda * dyda;
+    let odlen = dxod * dxod + dyod * dyod;
+    // Find the square of the length of the longest edge.
+    let maxlen = if dalen > oalen { dalen } else { oalen };
+    let maxlen = if odlen > maxlen { odlen } else { maxlen };
+    maxlen > 0.05 * (triorg[0] * triorg[0] + triorg[1] * triorg[1]) + 0.02
+}
+
+/// Convenience constant to help only fill the required fields using
+/// the struct update syntax.
+pub const EMPTY_TRIANGULATEIO: triangulateio = triangulateio {
+    pointlist: ptr::null_mut(),
+    pointattributelist: ptr::null_mut(),
+    pointmarkerlist: ptr::null_mut(),
+    numberofpoints: 0,
+    numberofpointattributes: 0,
+    trianglelist: ptr::null_mut(),
+    triangleattributelist: ptr::null_mut(),
+    trianglearealist: ptr::null_mut(),
+    neighborlist: ptr::null_mut(),
+    numberoftriangles: 0,
+    numberofcorners: 3,
+    numberoftriangleattributes: 0,
+    segmentlist: ptr::null_mut(),
+    segmentmarkerlist: ptr::null_mut(),
+    numberofsegments: 0,
+    holelist: ptr::null_mut(),
+    numberofholes: 0,
+    regionlist: ptr::null_mut(),
+    numberofregions: 0,
+    edgelist: ptr::null_mut(),
+    edgemarkerlist: ptr::null_mut(),
+    normlist: ptr::null_mut(),
+    numberofedges: 0,
+};
+
+
+#[cfg(test)]
+mod test {
+    use super::triangulateio;
+
+    #[test]
+    fn test_triunsuitable() {
+        let triswitches = b"zpDu"; // u → triunsuitable
+        let square = [(0.,0.), (1.,0.), (1.,1.), (0.,1.)];
+        let segments = [(0,1), (1,2), (2,3), (3,0)];
+        let input = triangulateio {
+            pointlist: square.as_ptr() as *mut _,
+            numberofpoints: square.len() as i32,
+            segmentlist: segments.as_ptr() as *mut _,
+            numberofsegments: segments.len() as i32,
+            .. super::EMPTY_TRIANGULATEIO
+        };
+        let mut output = triangulateio {
+            .. super::EMPTY_TRIANGULATEIO
+        };
+        let mut vorout = triangulateio {
+            .. super::EMPTY_TRIANGULATEIO
+        };
+        unsafe {
+            super::triangulate(triswitches.as_ptr() as *const i8,
+                               &input, &mut output, &mut vorout);
+        }
+    }
 }
